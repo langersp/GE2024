8886d84590b31d3fdd48036f2227eaa9586d3da8
Paul Langley <paull@thomas.co>
2023-09-19 21:29:58 +0100

    tweak

--- app/components/SideBarHeader.js
+++ app/components/SideBarHeader.js
@@ -6,9 +6,9 @@ const SideBarHeader = () => {
       <div className="header-title">
         <h3>5654 ELECTION CENTRE</h3>
       </div>
-      {/* <button class="menu-burger">
-        <img src="Menu_Button_2.svg" alt="Menu Burger" />
-      </button> */}
+      <button class="menu-burger">
+        {/* <img src="Menu_Button_2.svg" alt="Menu Burger" /> */}
+      </button>
     </div>
   );
 };
