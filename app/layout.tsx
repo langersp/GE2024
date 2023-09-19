1509a22eb73ff6768898a98c52a9bf029dbe585d
Paul Langley <paull@thomas.co>
2023-09-19 21:18:13 +0100

    Add fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,11 @@
 import './globals.css'
 import './style.css'
+import './use.typekit.net_oty8kzu.css'
 
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+//import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+//const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'GE2024',
@@ -18,7 +19,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
   )
 }
