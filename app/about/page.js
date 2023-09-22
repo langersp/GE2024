aef68f973604789127ce3116c7518f04d187eccf
Paul Langley <paull@thomas.co>
2023-09-22 10:35:07 +0100

    About amends

--- app/about/page.js
+++ app/about/page.js
@@ -19,11 +19,9 @@ export default function Page() {
 
               <h2>ABOUT OUR GENERAL ELECTION PREDICTOR</h2>
 
-              <p>Lorem ipsum ante aenean amet hac etiam felis non augue suscipit tristique, condimentum dui vulputate pretium sapien rutrum quisque quam placerat.</p>
-
               <div className="links">
 
-                <a href="#" className="link">Related Blogs</a>
+                <a href="/blog" className="link">Related Blogs</a>
 
               </div>
 
@@ -36,17 +34,17 @@ export default function Page() {
 
           <div className="about-text-container">
 
-            <h4>About our general election centre:</h4>
+            {/* <h4>About our general election centre:</h4> */}
 
 
-            <p>National polls offer an insight into the mood of the public. But they can only ever provide a snapshot, not a prediction, about what will happen at the General Election.</p>
+            <p><strong>National polls offer an insight into the mood of the public. But they can only ever provide a snapshot, not a prediction, about what will happen at the General Election.</strong></p>
             <p>We have created this tool to help you test your own assumptions about what might happen at the general election. It is based on the latest YouGov polling into how the public say they expect to vote in England, Scotland and Wales. It is designed to help you understand the likely scenarios should the polls tighten, the role tactical voting could play and some of the other factors that will influence the final result.</p>
 
 
 
             <div className="quote">
 
-              <blockquote>&quote;Lorem ipsum ante aenean amet hac etiam felis non augue suscipit tristique, condimentum dui vulputate pretium sapien rutrum quisque quam placerat. Dictum massa himenaeos nulla lacinia dictum velit metus erat.&quote;</blockquote>
+              <blockquote>&quot;The next general election is now firmly in sight. Current polling suggests Labour are heading to victory - yet the political arithmetic remains extraordinarily difficult for Starmer.  Since 2010, the UK has experienced four different models of government: a coalition government; a government with a small working majority; a minority government; and, since 2019, a government with a strong working majority.  Sir Keir Starmer is likely to be the UK’s next Prime Minister – managing a Labour-led administration. But the evidence suggests the model of government he will preside over – and the dynamics in parliament – are harder to determine.&quot;</blockquote>
 
               <span className="author">Ben Thornton - Founding Partner</span>
 
--- app/components/SideBarFooter.js
+++ app/components/SideBarFooter.js
@@ -3,6 +3,19 @@
 const SideBarFooter = () => {
   return (
     <div className="sidebar-footer">
+      <div className="links">
+        <a href="https://www.5654.co.uk" target="_blank" className="link">
+          Visit Our Website
+        </a>
+
+        <a
+          href="https://www.5654.co.uk/contact-us"
+          target="_blank"
+          className="link"
+        >
+          Contact Us
+        </a>
+      </div>
       <img src="../Footer_Logo.svg" alt="Logo" />
 
       <img
