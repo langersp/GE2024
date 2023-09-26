c6528122c4c1a4bf07954758cc93eb71660a7097
Paul Langley <paull@thomas.co>
2023-09-26 15:49:08 +0100

    blog mobile

--- app/blog/page.js
+++ app/blog/page.js
@@ -18,7 +18,7 @@ export default function Page() {
   return (
     <>
       <section className="blog-section content-with-sidebar">
-        <div className={`sidebar ${menuState ? 'menu-active' : ''}`}>
+        <div className={`sidebar ${menuState ? "menu-active" : ""}`}>
           <SideBarHeader handleMenu={handleMenu} />
           <div className="sidebar-main-container">
             <div className="sidebar-main-content">
@@ -38,6 +38,10 @@ export default function Page() {
           </div>
         </div>
 
+        <div className="header-section mobile-only">
+          <SideBarHeader handleMenu={handleMenu} />
+        </div>
+
         <div className="main-content">
           <div className="blog-tiles-section">
             <div className="blog-tiles">
@@ -46,12 +50,18 @@ export default function Page() {
                   <img src="../blog1.jpeg" alt="Blog Image" />
                 </div>
 
-                <a href="https://www.5654.co.uk/our-perspective/is-a-labour-majority-inevitable" target="_blank" className="blog-link"></a>
+                <a
+                  href="https://www.5654.co.uk/our-perspective/is-a-labour-majority-inevitable"
+                  target="_blank"
+                  className="blog-link"
+                ></a>
 
                 <div className="blog-excerpt">
                   <h3>Is a Labour Majority Inevitable?</h3>
                   <p>
-                  At exactly 10pm on General Election night 2019, the BBC, ITV and Sky published their shared Exit poll revealing that Boris Johnson... 
+                    At exactly 10pm on General Election night 2019, the BBC, ITV
+                    and Sky published their shared Exit poll revealing that
+                    Boris Johnson...
                   </p>
                 </div>
               </div>
@@ -61,12 +71,18 @@ export default function Page() {
                   <img src="../blog2.jpeg" alt="Blog Image" />
                 </div>
 
-                <a href="https://www.5654.co.uk/our-perspective/the-switchers-and-the-undecideds" target="_blank" className="blog-link"></a>
+                <a
+                  href="https://www.5654.co.uk/our-perspective/the-switchers-and-the-undecideds"
+                  target="_blank"
+                  className="blog-link"
+                ></a>
 
                 <div className="blog-excerpt">
                   <h3>The Switchers and the Undecideds</h3>
                   <p>
-                  With the national polls indicating a Labour resurgence, 5654 & Company wanted to understand the extent to which voters are switching...
+                    With the national polls indicating a Labour resurgence, 5654
+                    & Company wanted to understand the extent to which voters
+                    are switching...
                   </p>
                 </div>
               </div>
@@ -76,63 +92,29 @@ export default function Page() {
                   <img src="../blog3.jpeg" alt="Blog Image" />
                 </div>
 
-                <a href="https://www.5654.co.uk/our-perspective/think-tanks-that-matter" target="_blank" className="blog-link"></a>
+                <a
+                  href="https://www.5654.co.uk/our-perspective/think-tanks-that-matter"
+                  target="_blank"
+                  className="blog-link"
+                ></a>
 
                 <div className="blog-excerpt">
-                  <h3>What Businesses Should be Doing Now: Think Tanks That Matter</h3>
+                  <h3>
+                    What Businesses Should be Doing Now: Think Tanks That Matter
+                  </h3>
                   <p>
-                  They are the policy engine for the main parties. There is often a cross-transfer of people - policy strategists may leave think-tanks...                  </p>
+                    They are the policy engine for the main parties. There is
+                    often a cross-transfer of people - policy strategists may
+                    leave think-tanks...{" "}
+                  </p>
                 </div>
               </div>
             </div>
-
-            {/* <div className="blog-tiles">
-              <div className="blog-tile">
-                <div className="blog-img">
-                  <img src="../Background_Image.jpg" alt="Blog Image" />
-                </div>
-
-                <a href="#" className="blog-link"></a>
-
-                <div className="blog-excerpt">
-                  <p>
-                    Lorem ipsum ante aenean amet hac etiam felis non augue
-                    suscipit tristique, condimentum dui.
-                  </p>
-                </div>
-              </div>
-
-              <div className="blog-tile">
-                <div className="blog-img">
-                  <img src="../Background_Image.jpg" alt="Blog Image" />
-                </div>
-
-                <a href="#" className="blog-link"></a>
-
-                <div className="blog-excerpt">
-                  <p>
-                    Lorem ipsum ante aenean amet hac etiam felis non augue
-                    suscipit tristique, condimentum dui.
-                  </p>
-                </div>
-              </div>
-
-              <div className="blog-tile">
-                <div className="blog-img">
-                  <img src="../Background_Image.jpg" alt="Blog Image" />
-                </div>
-
-                <a href="#" className="blog-link"></a>
-
-                <div className="blog-excerpt">
-                  <p>
-                    Lorem ipsum ante aenean amet hac etiam felis non augue
-                    suscipit tristique, condimentum dui.
-                  </p>
-                </div>
-              </div>
-            </div> */}
           </div>
+        </div>
+
+        <div className="footer-section mobile-only">
+          <SideBarFooter />
         </div>
       </section>
     </>
